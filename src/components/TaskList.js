95a6c5e1132610abeebcecea5f8ef0dab3ff6f74
lucasfarmwald <farmwaldlucas@gmail.com>
2022-02-05 22:24:35 -0600

    finished Task and TaskList

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,15 +1,17 @@
+
 import React from "react";
-import Task from "./Task"
+import Task from "./Task";
 
-function TaskList(taskData) {
-
-
-
-  return (
-    <div className="tasks">
-      <Task taskData={taskData} />
-    </div>
-  );
+function TaskList({ tasks, onDeleteTask }) {
+  const taskList = tasks.map((task) => (
+    <Task
+      key={task.text}
+      text={task.text}
+      category={task.category}
+      onDeleteTask={onDeleteTask}
+    />
+  ));
+  return <div className="tasks">{taskList}</div>;
 }
 
 export default TaskList;
